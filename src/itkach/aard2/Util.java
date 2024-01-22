4f303094317db438c12563c2603f17920039f965
Meenbeese <meenbeese@tutanota.com>
2024-01-22 16:10:21 -0500

    Remove huge unnecessary doc

--- src/itkach/aard2/Util.java
+++ src/itkach/aard2/Util.java
@@ -29,23 +29,6 @@ class Util {
             Collections.sort(list, comparator);
         }
         catch(Exception e) {
-            // From http://www.oracle.com/technetwork/java/javase/compatibility-417013.html#source
-            /*
-            Synopsis: Updated sort behavior for Arrays and Collections may throw an IllegalArgumentException
-            Description: The sorting algorithm used by java.util.Arrays.sort and (indirectly) by
-                         java.util.Collections.sort has been replaced. The new sort implementation may
-                         throw an IllegalArgumentException if it detects a Comparable that violates
-                         the Comparable contract. The previous implementation silently ignored such a situation.
-                         If the previous behavior is desired, you can use the new system property,
-                         java.util.Arrays.useLegacyMergeSort, to restore previous mergesort behavior.
-            Nature of Incompatibility: behavioral
-            RFE: 6804124
-             */
-            // Name comparators use ICU collation key comparison. Given Unicode collation complexity
-            // It's hard to be sure that collation key comparisons won't trigger an exception. It certainly
-            // Does at least for some keys in ICU 53.1.
-            // Incorrect or no sorting seems preferable than a crashing app.
-            // TODO: perhaps java.util.Collections.sort shouldn't be used at all
             Log.w(TAG, "Error while sorting:", e);
         }
     }
